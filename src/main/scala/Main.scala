e34881c00f13131877115badede9f25fbc00a959
Thales Rodolfo <thalesrodolfo@protonmail.com>
2021-12-08 10:56:18 -0300

    Remove code of Main file (unused)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,3 @@
 import scala.annotation.tailrec
-object Main extends App {
 
-  val inputs:List[Int] = 
-    List (199,
-          200,
-          208,
-          210,
-          200,
-          207,
-          240,
-          269,
-          260,
-          263)
-
-  def getHead(l: List[Int]): Int = if (l.size > 0) l.head else 0
-    
-  @tailrec
-  def execute(inputs: List[Int], result: List[Boolean]): List[Boolean] = {
-    if (inputs.size == 0) result
-
-    val newList = inputs.drop(1)
-    val valueIncreased = getHead(inputs) < getHead(newList)
-
-    execute(newList, valueIncreased :: result)
-  }
-
-  val result = execute(inputs, List()).filter(p => p).size
-
-  println(result)
-}
+object Main extends App {}
